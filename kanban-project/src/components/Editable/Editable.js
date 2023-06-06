1e0c6f1dbae50cab437e174ffa1025e09cb42ba6
unknown <singh777komal@gmail.com>
2023-06-06 20:47:01 +0530

    completedUI

--- kanban-project/src/components/Editable/Editable.js
+++ kanban-project/src/components/Editable/Editable.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { X } from 'react-feather'
+import './Editable.css'
 
 export default function Editable(props) {
+
+    const [showEdit, setShowEdit] = useState(false)
   return (
     <div className='editable'>
-        <p>{props.text || "Add item"}</p>
-        <form className='editable_edit'
+
+        {
+            showEdit ? 
+            <form className={`editable_edit ${props.editCase || "" }`}
         onSubmit={(e)=>{
             e.preventDefault()
             if(props.onSubmit)props.onSubmit()
         }}
         >
-            <input type='text' placeholder={props.placeholder} defaultValue={props.text}/>
+            <input type='text'
+             placeholder={props.placeholder || "Enter item"} 
+             defaultValue={props.text}
+
+             />
             <div className='editable_edit_footer'>
                 <button type='submit'>
                     {props.buttonText || "Add"}
                 </button>
-                <X />
+                <X onClick={()=>setShowEdit(false)}/>
             </div>
         </form>
+        :(
+        <p className={`editable_display ${props.displayClass || ""}`} 
+        onClick={()=>setShowEdit(true)}>{props.text || "Add item"}</p>
+        )
+        }
+       
     </div>
   )
 }
+
+
+
+
+
